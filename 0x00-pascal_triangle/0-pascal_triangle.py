e71776294dae4eb345c57cc91fe9ce907c33ee8a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-26 02:41:54 +0100

    pascal triangle explained

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,12 +14,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
+    # start with triangle, a list of list
     triangle = [[1]]
 
+    # for each row number  from 1 to n -1  start with 1
     for row in range(1, n):
         new_row = [1]
 
         # calculate middle element
+        # for each element index from 1 to
+        # (length of previous row - 1):
         for i in range(1, len(triangle[row - 1])):
             new_element = triangle[row - 1][i - 1] + triangle[row - 1][i]
             new_row.append(new_element)
