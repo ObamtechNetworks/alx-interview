adf3ed90953c6f1e43e7816fecd86e8fe9d3471e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-06 04:24:42 +0100

    fixed all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
 const ID = process.argv[2];
+
+if (!ID) {
+  console.log('Error: Missing movie ID argument.');
+  process.exit(1);
+}
+
 const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
 function getCharacter (listChar, index) {
+  if (!Array.isArray(listChar)) {
+    console.log("Error: 'listChar' is not an array.");
+    return;
+  }
+
   if (index === listChar.length) {
     return;
   }
+
   request(`${listChar[index]}`, (error, response, body) => {
     if (error) {
       console.log(error);
@@ -24,6 +36,11 @@ request(url, (error, response, body) => {
   } else {
     const film = JSON.parse(response.body);
     const listChar = film.characters;
-    getCharacter(listChar, 0);
+
+    if (Array.isArray(listChar)) {
+      getCharacter(listChar, 0);
+    } else {
+      console.log("Error: 'characters' is not an array or is undefined.");
+    }
   }
 });
