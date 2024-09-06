2a4b8b62799663b262d5141811622796c39e3b6d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-06 03:43:35 +0100

    fix later

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,37 @@
 #!/usr/bin/node
-const request = require("request");
-const ID = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
-function getCharacter(listChar, index) {
-  if (index === listChar.length) {
+const request = require("request");
+
+// Get the Movie ID from the command-line argument
+const movieId = process.argv[2];
+
+// Define the API endpoint with the Movie ID
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+// Make the request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
     return;
   }
-  request(`${listChar[index]}`, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const character = JSON.parse(response.body);
-      console.log(character.name);
-      getCharacter(listChar, index + 1);
-    }
-  });
-}
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const film = JSON.parse(response.body);
-    const listChar = film.characters;
-    getCharacter(listChar, 0);
-  }
+  // Parse the response body as JSON
+  const filmData = JSON.parse(body);
+
+  // Get the array of character URLs from the response
+  const characters = filmData.characters;
+
+  // For each character URL, make an API call to get the character name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      // Parse the character data and print the character name
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
