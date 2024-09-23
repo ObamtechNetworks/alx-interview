29db39755c19cd2944e2261fd4e16d3f5fe9b01b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-23 03:00:33 +0100

    used greedy method

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
-""" The coin change problem
-To solve the change problem using dynamic programming.
-"""
+"""The coin change problem using a greedy algorithm."""
 
 from typing import List
 
 
 def makeChange(coins: List[int], total: int) -> int:
     """Determines the fewest number of coins needed to meet
-    a given amount total.
+    a given amount total using a greedy algorithm.
+
     Args:
-        coins (List[int]): list of coins denomination
+        coins (List[int]): list of coin denominations
         total (int): the given target amount to meet
+
     Returns:
-       int: the fewest number of coins needed to meet
+        int: the fewest number of coins needed to meet
         a given amount total
     """
+    # If total is less than or equal to 0, no coins are needed
     if total <= 0:
         return 0
-    # Initialize the dp array with a size of total + 1
-    dp = [float("inf")] * (total + 1)  # set all elements to infintity
-    dp[0] = 0  # No coins needed to make 0
 
-    # Fill the dp table
-    for c in coins:
-        for i in range(c, total + 1):
-            dp[i] = min(dp[i], dp[i - c] + 1)
-    # Return resultvcheck dp[total]. If it's still infinity
-    # it means the total cannot be reached with the given coins,
-    # so return -1. Otherwise, return dp[total].
-    return dp[total] if dp[total] != float('inf') else -1
+    # Sort coins in desc order to attempt the largest denomination first
+    coins.sort(reverse=True)
+
+    fewest = 0  # Counter for the fewest number of coins
+
+    # Iterate over each coin denomination
+    for coin in coins:
+        if total == 0:
+            break  # If total becomes 0, break out of the loop
+
+        # Add the max number of coins of this denomination that can be used
+        if coin <= total:
+            # Calculate how many coins of this denomination
+            num_of_coins = total // coin
+            # Increment the fewest count by that number
+            fewest += num_of_coins
+            # Decrease the total by the equivalent value
+            total -= num_of_coins * coin
+
+    # If total is not 0 after the loop, return -1 (can't make exact change)
+    return fewest if total == 0 else -1
